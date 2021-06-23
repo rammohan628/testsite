45c12b3c95062fa192ac8c2b7f8c9d1544c57d69
rammohan503 <rammohan503@gmail.com>
2021-06-23 16:01:50 +0530

    Commit 2

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 
 $email = "rammohan503@gmail.com";
 $subject =  "Email Test";
-$message = "this is a mail testing email function on server Hello";
+$message = "this is a mail testing email function on server Hello 1234";
 
 
 $sendMail = mail($email, $subject, $message);
